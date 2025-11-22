1614d76ca27c1a5b55768f19e5f31c46ecd79e3e
MRAMin0 <aminmonajati9@gmail.com>
2025-11-22 12:17:42 +0330

    change the internet connection checker package

--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -1,14 +1,19 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
+// تغییر ۱: ایمپورت پکیج جدید (Plus)
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class NetworkService {
   static Future<bool> hasInternet() async {
     final connectivity = await Connectivity().checkConnectivity();
+
+    // این بخش مربوط به Connectivity Plus است و کاملا درست است
     if (connectivity.isEmpty ||
         connectivity.contains(ConnectivityResult.none)) {
       return false;
     }
 
-    return await InternetConnectionChecker().hasConnection;
+    // تغییر ۲ و ۳: استفاده از کلاس و متد جدید
+    // این متد در وب هم کار می‌کند و دیگر کرش نمی‌کند
+    return await InternetConnection().hasInternetAccess;
   }
 }
